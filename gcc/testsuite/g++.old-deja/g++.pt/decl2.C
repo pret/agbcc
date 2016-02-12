166e94b61c7150981753e2ad54f1d55149e5f0c7
YamaArashi <shadow962@live.com>
2016-02-12 10:34:13 -0800

    remove g++ test cases

--- gcc/testsuite/g++.old-deja/g++.pt/decl2.C
+++
@@ -1,9 +0,0 @@
-// Build don't link:
-
-// Simplified from testcase by Christophe Boyanique <boyan@imac.u-paris2.fr>
-
-// crash test - XFAIL *-*-*
-
-template <class T> struct foo { foo(); };
-template<class T> foo<T>::foo() {}
-T // ERROR - currently an ICE
