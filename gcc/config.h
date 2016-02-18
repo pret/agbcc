7d46fa8f8f179c38d1ac777e72a381a46a3c5969
YamaArashi <shadow962@live.com>
2016-02-18 06:51:16 -0800

    delete xm-i386.h

--- gcc/config.h
+++ gcc/config.h
@@ -1,12 +1,14 @@
 #include "gansidecl.h"
-#include "i386/xm-i386.h"
+
+/* #defines that need visibility everywhere.  */
+#define FALSE 0
+#define TRUE 1
+
+/* This describes the machine the compiler is hosted on.  */
+#define HOST_BITS_PER_CHAR 8
+#define HOST_BITS_PER_SHORT 16
+#define HOST_BITS_PER_INT 32
+#define HOST_BITS_PER_LONG 32
+#define HOST_BITS_PER_LONGLONG 64
+
 #include "arm/telf.h"
-#ifndef HAVE_ATEXIT
-#define HAVE_ATEXIT
-#endif
-#ifndef POSIX
-#define POSIX
-#endif
-#ifndef BSTRING
-#define BSTRING
-#endif
