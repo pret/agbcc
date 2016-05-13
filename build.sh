d26e6645864c5bb25a4465a3c32f74f4341df73c
YamaArashi <shadow962@live.com>
2016-05-12 20:09:19 -0700

    install old_agbcc too

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 make -C gcc clean
 make -C gcc old
-make -C libgcc clean
-make -C libgcc
+mv gcc/old_agbcc .
 make -C gcc clean
 make -C gcc
+mv gcc/agbcc .
+make -C libgcc clean
+make -C libgcc
+mv libgcc/libgcc.a .
--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@ if [ "$1" != "" ]; then
     mkdir -p $1/tools/agbcc/bin
     mkdir -p $1/tools/agbcc/include
     mkdir -p $1/tools/agbcc/lib
-    cp gcc/agbcc $1/tools/agbcc/bin/
+    cp agbcc $1/tools/agbcc/bin/
+    cp old_agbcc $1/tools/agbcc/bin/
     cp ginclude/* $1/tools/agbcc/include/
-    cp libgcc/libgcc.a $1/tools/agbcc/lib/
+    cp libgcc.a $1/tools/agbcc/lib/
 else
     echo "Usage: install.sh PATH"
 fi
