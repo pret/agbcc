dce943da0f089d7c8535418049a79f4bd483449b
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2019-08-06 20:40:43 -0300

    Fix Makefiles to account for the $CC and $CXX environment

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 set -e
+CCOPT=
+CXXOPT=
+if [ ! -z "$CC" ]; then CCOPT=CC=$CC; fi
+if [ ! -z "$CXX" ]; then CXXOPT=CXX=$CXX; fi
 make -C gcc clean
-make -C gcc old
+make -C gcc old $CCOPT $CXXOPT
 mv gcc/old_agbcc .
 make -C gcc clean
-make -C gcc
+make -C gcc $CCOPT $CXXOPT
 mv gcc/agbcc .
 make -C libgcc clean
-make -C libgcc
+make -C libgcc $CCOPT $CXXOPT
 mv libgcc/libgcc.a .
 make -C libc clean
-make -C libc
+make -C libc $CCOPT $CXXOPT
 mv libc/libc.a .
