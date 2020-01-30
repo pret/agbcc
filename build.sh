4f87fae05b87cefd4f8fc0b2b18e639b0fad25fc
camthesaxman <camthesaxman@users.noreply.github.com>
2020-01-30 02:06:21 -0600

    make clean is broken

--- build.sh
+++ build.sh
@@ -3,16 +3,15 @@ set -e
 CCOPT=
 CXXOPT=
 
-if [ ! -z "$CC" ]; then CCOPT=CC=$CC; fi
-if [ ! -z "$CXX" ]; then CXXOPT=CXX=$CXX; fi
-make -C gcc clean
-make -C gcc old $CCOPT $CXXOPT
-mv gcc/old_agbcc .
-make -C gcc clean
-make -C gcc $CCOPT $CXXOPT
-mv gcc/agbcc .
+#if [ ! -z "$CC" ]; then CCOPT=CC=$CC; fi
+#if [ ! -z "$CXX" ]; then CXXOPT=CXX=$CXX; fi
+#make -C gcc clean
+#make -C gcc old $CCOPT $CXXOPT
+#mv gcc/old_agbcc .
+#make -C gcc clean
+#make -C gcc $CCOPT $CXXOPT
+#mv gcc/agbcc .
 # not sure if the ARM compiler is the old one or the new one (-DOLD_COMPILER)
-[ ! -e gcc_arm/Makefile ] || make -C gcc_arm clean
 rm -f gcc_arm/config.status gcc_arm/config.cache
 cd gcc_arm && ./configure --target=arm-elf --host=i386-linux-gnu && make cc1 && cd ..
 mv gcc_arm/cc1 agbcc_arm
