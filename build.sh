b5ed196b51d861447c20a0a2bea105513d27c70f
Missingmew <nintendo6496@googlemail.com>
2020-12-01 03:40:39 +0100

    revert build.sh to make without -j option, adjust build.sh and install.sh to work with the same environment the makefile uses

--- build.sh
+++ build.sh
@@ -5,20 +5,23 @@ CXXOPT=
 
 if [ ! -z "$CC" ]; then CCOPT=CC=$CC; fi
 if [ ! -z "$CXX" ]; then CXXOPT=CXX=$CXX; fi
-make -C gcc clean
-make -C gcc -j$(nproc) old $CCOPT $CXXOPT
-mv gcc/old_agbcc .
-make -C gcc clean
-make -C gcc -j$(nproc) normal $CCOPT $CXXOPT
-mv gcc/agbcc .
+
+mkdir -p build_old_agbcc
+srcdir=../gcc make -C build_old_agbcc -f ../gcc/Makefile clean
+srcdir=../gcc make -C build_old_agbcc -f ../gcc/Makefile old $CCOPT $CXXOPT
+
+mkdir -p build_agbcc
+srcdir=../gcc make -C build_agbcc -f ../gcc/Makefile clean
+srcdir=../gcc make -C build_agbcc -f ../gcc/Makefile normal $CCOPT $CXXOPT
+
 # not sure if the ARM compiler is the old one or the new one (-DOLD_COMPILER)
-make -C gcc_arm distclean
+# || true is needed to keep going if the distclean fails, such as when no configure has been done before
+make -C gcc_arm distclean || true
 cd gcc_arm && ./configure --target=arm-elf --host=i386-linux-gnu && cd ..
-make -C gcc_arm -j$(nproc) cc1
-mv gcc_arm/cc1 agbcc_arm
+make -C gcc_arm cc1
+
 make -C libgcc clean
-make -C libgcc -j$(nproc) $CCOPT $CXXOPT
-mv libgcc/libgcc.a .
+make -C libgcc $CCOPT $CXXOPT
+
 make -C libc clean
-make -C libc -j$(nproc) $CCOPT $CXXOPT
-mv libc/libc.a .
+make -C libc $CCOPT $CXXOPT
--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@ if [ "$1" != "" ]; then
     mkdir -p $1/tools/agbcc/bin
     mkdir -p $1/tools/agbcc/include
     mkdir -p $1/tools/agbcc/lib
-    cp agbcc $1/tools/agbcc/bin/
-    cp old_agbcc $1/tools/agbcc/bin/
-    cp agbcc_arm $1/tools/agbcc/bin/
+    cp build_agbcc/agbcc $1/tools/agbcc/bin/
+    cp build_old_agbcc/old_agbcc $1/tools/agbcc/bin/
+    cp gcc_arm/cc1 $1/tools/agbcc/bin/agbcc_arm
     cp -R libc/include $1/tools/agbcc/ #drop include, because we don't want include/include
     cp ginclude/* $1/tools/agbcc/include/
-    cp libgcc.a $1/tools/agbcc/lib/
-    cp libc.a $1/tools/agbcc/lib/
+    cp libgcc/libgcc.a $1/tools/agbcc/lib/
+    cp libc/libc.a $1/tools/agbcc/lib/
 else
     echo "Usage: install.sh PATH"
 fi
