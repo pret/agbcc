2bcc111fd9917160696c680bcab744f042f77999
Missingmew <nintendo6496@googlemail.com>
2020-12-01 01:02:54 +0100

    fix dependencies in agbcc makefile to allow parallel builds

--- build.sh
+++ build.sh
@@ -6,18 +6,19 @@ CXXOPT=
 if [ ! -z "$CC" ]; then CCOPT=CC=$CC; fi
 if [ ! -z "$CXX" ]; then CXXOPT=CXX=$CXX; fi
 make -C gcc clean
-make -C gcc old $CCOPT $CXXOPT
+make -C gcc -j$(nproc) old $CCOPT $CXXOPT
 mv gcc/old_agbcc .
 make -C gcc clean
-make -C gcc $CCOPT $CXXOPT
+make -C gcc -j$(nproc) normal $CCOPT $CXXOPT
 mv gcc/agbcc .
 # not sure if the ARM compiler is the old one or the new one (-DOLD_COMPILER)
-rm -f gcc_arm/config.status gcc_arm/config.cache
-cd gcc_arm && ./configure --target=arm-elf --host=i386-linux-gnu && make cc1 && cd ..
+make -C gcc_arm distclean
+cd gcc_arm && ./configure --target=arm-elf --host=i386-linux-gnu && cd ..
+make -C gcc_arm -j$(nproc) cc1
 mv gcc_arm/cc1 agbcc_arm
 make -C libgcc clean
-make -C libgcc $CCOPT $CXXOPT
+make -C libgcc -j$(nproc) $CCOPT $CXXOPT
 mv libgcc/libgcc.a .
 make -C libc clean
-make -C libc $CCOPT $CXXOPT
+make -C libc -j$(nproc) $CCOPT $CXXOPT
 mv libc/libc.a .
