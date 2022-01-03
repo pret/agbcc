de7ccf2a4313398fb29133ad78c5547805233666
luckytyphlosion <10688458+luckytyphlosion@users.noreply.github.com>
2022-01-03 11:18:12 -0500

    Add helpful error checks for common user errors.

--- install.sh
+++ install.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 set -e
 if [ "$1" != "" ]; then
-    mkdir -p $1/tools/agbcc
-    mkdir -p $1/tools/agbcc/bin
-    mkdir -p $1/tools/agbcc/include
-    mkdir -p $1/tools/agbcc/lib
-    cp agbcc $1/tools/agbcc/bin/
-    cp old_agbcc $1/tools/agbcc/bin/
-    cp agbcc_arm $1/tools/agbcc/bin/
-    cp -R libc/include $1/tools/agbcc/ #drop include, because we don't want include/include
-    cp ginclude/* $1/tools/agbcc/include/
-    cp libgcc.a $1/tools/agbcc/lib/
-    cp libc.a $1/tools/agbcc/lib/
+	if [ -d "$1" ]; then
+		mkdir -p $1/tools/agbcc
+		mkdir -p $1/tools/agbcc/bin
+		mkdir -p $1/tools/agbcc/include
+		mkdir -p $1/tools/agbcc/lib
+		cp agbcc $1/tools/agbcc/bin/
+		cp old_agbcc $1/tools/agbcc/bin/
+		cp agbcc_arm $1/tools/agbcc/bin/
+		cp -R libc/include $1/tools/agbcc/ #drop include, because we don't want include/include
+		cp ginclude/* $1/tools/agbcc/include/
+		cp libgcc.a $1/tools/agbcc/lib/
+		cp libc.a $1/tools/agbcc/lib/
+	else
+		echo "Target directory does not exist. Did you mean to do \"./install.sh ../$1\"?"
+	fi
 else
     echo "Usage: install.sh PATH"
 fi
