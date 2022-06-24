fe7a192d3ceb4ebdf7ef1f13459cb3ff9772f46f
Colton G. Rushton <colton51919@gmail.com>
2022-06-24 16:23:54 -0300

    Restore gcc_arm configure artifacts, deleted in seprarate PR

---
+++ gcc_arm/config.h
@@ -0,0 +1,12 @@
+#include "auto-host.h"
+#include "gansidecl.h"
+#include "i386/xm-i386.h"
+#ifndef HAVE_ATEXIT
+#define HAVE_ATEXIT
+#endif
+#ifndef POSIX
+#define POSIX
+#endif
+#ifndef BSTRING
+#define BSTRING
+#endif
