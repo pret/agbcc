3a31b19fadd53288872abfb8cace2bbd1996e2e3
Colton G. Rushton <colton51919@gmail.com>
2022-06-24 13:14:50 -0300

    Fix parallel builds

--- build.sh
+++ build.sh
@@ -3,27 +3,22 @@ set -e
 CCOPT=
 CXXOPT=
 
-# error if devkitarm is not installed and binutils-arm-none-eabi is not installed
-if ! ([ -n "$DEVKITARM" ] && [ -d "$DEVKITARM/bin" ]) && ! (command -v arm-none-eabi-as &> /dev/null && command -v arm-none-eabi-ar &> /dev/null) ; then
-	echo "Could not find a binutils installation! Re-read the instructions and make sure you've installed either devkitARM or binutils-arm-none-eabi, depending on your system."
-	exit 1
-fi
-
 if [ ! -z "$CC" ]; then CCOPT=CC=$CC; fi
 if [ ! -z "$CXX" ]; then CXXOPT=CXX=$CXX; fi
 make -C gcc clean
-make -C gcc old $CCOPT $CXXOPT
+make -C gcc -j$(nproc) old $CCOPT $CXXOPT
 mv gcc/old_agbcc .
 make -C gcc clean
-make -C gcc $CCOPT $CXXOPT
+make -C gcc -j$(nproc) normal $CCOPT $CXXOPT
 mv gcc/agbcc .
 # not sure if the ARM compiler is the old one or the new one (-DOLD_COMPILER)
-rm -f gcc_arm/config.status gcc_arm/config.cache
-cd gcc_arm && ./configure --target=arm-elf --host=i386-linux-gnu && make cc1 && cd ..
+make -C gcc_arm distclean
+cd gcc_arm && ./configure --target=arm-elf --host=i386-linux-gnu && cd ..
+make -C gcc_arm -j$(nproc) cc1
 mv gcc_arm/cc1 agbcc_arm
 make -C libgcc clean
-make -C libgcc $CCOPT $CXXOPT
+make -C libgcc -j$(nproc) $CCOPT $CXXOPT
 mv libgcc/libgcc.a .
 make -C libc clean
-make -C libc $CCOPT $CXXOPT
+make -C libc -j$(nproc) $CCOPT $CXXOPT
 mv libc/libc.a .
