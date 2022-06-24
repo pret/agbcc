612c17383e94f311cfd3135af7cac23315ac239f
Colton G. Rushton <colton51919@gmail.com>
2022-06-24 14:09:33 -0300

    Add gcc_arm distclean to clean script

--- clean.sh
+++ clean.sh
@@ -1,4 +1,6 @@
 make -C gcc clean
+# || true is needed to keep going if the distclean fails, such as when no configure has been done before
+make -C gcc_arm distclean || true
 make -C libgcc clean
 make -C libc clean
 rm -f agbcc old_agbcc agbcc_arm libc.a libgcc.a
