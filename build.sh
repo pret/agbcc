fbd5d88e46c8883f474f6fa0acf14fae9fb0ea86
Colton G. Rushton <colton51919@gmail.com>
2022-07-01 15:21:01 -0300

    Remove nproc
    Because nproc can cause builds to stop entirely under operating systems that don't have it

--- build.sh
+++ build.sh
@@ -12,10 +12,10 @@ fi
 if [ ! -z "$CC" ]; then CCOPT=CC=$CC; fi
 if [ ! -z "$CXX" ]; then CXXOPT=CXX=$CXX; fi
 make -C gcc clean
-make -C gcc -j$(nproc) old $CCOPT $CXXOPT
+make -C gcc old $CCOPT $CXXOPT
 mv gcc/old_agbcc .
 make -C gcc clean
-make -C gcc -j$(nproc) normal $CCOPT $CXXOPT
+make -C gcc normal $CCOPT $CXXOPT
 mv gcc/agbcc .
 # not sure if the ARM compiler is the old one or the new one (-DOLD_COMPILER)
 # || true is needed to keep going if the distclean fails, such as when no configure has been done before
@@ -24,8 +24,8 @@ cd gcc_arm && ./configure --target=arm-elf --host=i386-linux-gnu && cd ..
 make -C gcc_arm cc1
 mv gcc_arm/cc1 agbcc_arm
 make -C libgcc clean
-make -C libgcc -j$(nproc) $CCOPT $CXXOPT
+make -C libgcc $CCOPT $CXXOPT
 mv libgcc/libgcc.a .
 make -C libc clean
-make -C libc -j$(nproc) $CCOPT $CXXOPT
+make -C libc $CCOPT $CXXOPT
 mv libc/libc.a .
