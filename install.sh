ee1420ffe9930542dfc3f7e455cfe78af22275a1
Mike Abbott <furrysalamander@gmail.com>
2023-03-13 14:21:01 -0600

    Modified the script so that the tools folder isn't part of the default install path.

--- install.sh
+++ install.sh
@@ -6,34 +6,40 @@ if [ "$1" = "-h" ]; then
 	exit 0
 fi
 
+# If there's no installation directory specified, the install script will default to 
+# installing agbcc into /opt/pret/agbcc
 if [ "$1" = "" ]; then
-	INSTALL_DIRECTORY=/opt/pret/
+	BASE_INSTALL_DIRECTORY=/opt/pret/
+	INSTALL_DIRECTORY=$BASE_INSTALL_DIRECTORY
 	mkdir -p $INSTALL_DIRECTORY
 else
-	INSTALL_DIRECTORY=$1
+	BASE_INSTALL_DIRECTORY=$1
+	INSTALL_DIRECTORY=$BASE_INSTALL_DIRECTORY/tools/
 fi
 
-if [ -d "$INSTALL_DIRECTORY" ]; then
-	mkdir -p $INSTALL_DIRECTORY/tools/agbcc
-	mkdir -p $INSTALL_DIRECTORY/tools/agbcc/bin
-	mkdir -p $INSTALL_DIRECTORY/tools/agbcc/include
-	mkdir -p $INSTALL_DIRECTORY/tools/agbcc/lib
-	cp agbcc $INSTALL_DIRECTORY/tools/agbcc/bin/
-	cp old_agbcc $INSTALL_DIRECTORY/tools/agbcc/bin/
-	cp agbcc_arm $INSTALL_DIRECTORY/tools/agbcc/bin/
-	cp -R libc/include $INSTALL_DIRECTORY/tools/agbcc/ #drop include, because we don't want include/include
-	cp ginclude/* $INSTALL_DIRECTORY/tools/agbcc/include/
-	cp libgcc.a $INSTALL_DIRECTORY/tools/agbcc/lib/
-	cp libc.a $INSTALL_DIRECTORY/tools/agbcc/lib/
+# The BASE_INSTALL_DIRECTORY nomenclature is so that the check for the existence of the 
+# directory doesn't fail just because there's no tools directory yet.
+if [ -d "$BASE_INSTALL_DIRECTORY" ]; then
+	mkdir -p $INSTALL_DIRECTORY/agbcc
+	mkdir -p $INSTALL_DIRECTORY/agbcc/bin
+	mkdir -p $INSTALL_DIRECTORY/agbcc/include
+	mkdir -p $INSTALL_DIRECTORY/agbcc/lib
+	cp agbcc $INSTALL_DIRECTORY/agbcc/bin/
+	cp old_agbcc $INSTALL_DIRECTORY/agbcc/bin/
+	cp agbcc_arm $INSTALL_DIRECTORY/agbcc/bin/
+	cp -R libc/include $INSTALL_DIRECTORY/agbcc/ #drop include, because we don't want include/include
+	cp ginclude/* $INSTALL_DIRECTORY/agbcc/include/
+	cp libgcc.a $INSTALL_DIRECTORY/agbcc/lib/
+	cp libc.a $INSTALL_DIRECTORY/agbcc/lib/
 	echo "agbcc successfully installed!"
 else
-	if [ -d "../$INSTALL_DIRECTORY" ]; then
+	if [ -d "../$BASE_INSTALL_DIRECTORY" ]; then
 		echo "Target directory does not exist. Did you mean to do \"./install.sh ../$1\"?"
 	else
-		if case $INSTALL_DIRECTORY in ".."*) true;; *) false;; esac; then
+		if case $BASE_INSTALL_DIRECTORY in ".."*) true;; *) false;; esac; then
 			echo "Target directory does not exist. If you aren't familiar with relative paths, make sure that agbcc and the repository are in the same directory, and run \"./install.sh $1\" again."
 		else
-			if echo "$INSTALL_DIRECTORY" | grep -qE '^[^/]*.$'; then
+			if echo "$BASE_INSTALL_DIRECTORY" | grep -qE '^[^/]*.$'; then
 				echo "Target directory does not exist. You probably meant to do \"./install.sh ../$1\", but agbcc and $1 do not exist in the same directory. Check your spelling, make sure that the repository has been cloned, ensure that agbcc and the repository are in the same directory, and run \"./install.sh ../$1\" again."
 			else
 				echo "Target directory does not exist. Check your spelling, re-read the instructions, and try again."
