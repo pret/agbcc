7a2074c01b868181ee1381583be7230405eb162c
Jan Helbling <janhelbling@laptop-home.my.domain>
2025-01-11 17:01:37 +0100

    Compatible with OpenBSD

--- build.sh
+++ build.sh
@@ -11,19 +11,26 @@ fi
 
 if [ ! -z "$CC" ]; then CCOPT=CC=$CC; fi
 if [ ! -z "$CXX" ]; then CXXOPT=CXX=$CXX; fi
-make -C gcc clean
-make -C gcc old $CCOPT $CXXOPT
+
+if [ "`uname`" == "OpenBSD" ];then
+	MAKE='gmake'
+else
+	MAKE='make'
+fi;
+
+${MAKE} -C gcc clean
+${MAKE} -C gcc old $CCOPT $CXXOPT
 mv gcc/old_agbcc .
-make -C gcc clean
-make -C gcc $CCOPT $CXXOPT
+${MAKE} -C gcc clean
+${MAKE} -C gcc $CCOPT $CXXOPT
 mv gcc/agbcc .
 # not sure if the ARM compiler is the old one or the new one (-DOLD_COMPILER)
 rm -f gcc_arm/config.status gcc_arm/config.cache
 cd gcc_arm && ./configure --target=arm-elf --host=i386-linux-gnu && make cc1 && cd ..
 mv gcc_arm/cc1 agbcc_arm
-make -C libgcc clean
-make -C libgcc $CCOPT $CXXOPT
+${MAKE} -C libgcc clean
+${MAKE} -C libgcc $CCOPT $CXXOPT
 mv libgcc/libgcc.a .
-make -C libc clean
-make -C libc $CCOPT $CXXOPT
+${MAKE} -C libc clean
+${MAKE} -C libc $CCOPT $CXXOPT
 mv libc/libc.a .
